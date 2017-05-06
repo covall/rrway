cf320b5899b23c8942bc231deb9d279627a1161d
Piotr Kowalczyk <piotr.m.kowalczyk@gmail.com>
2017-05-06 22:20:00 +0200

    Add react-reouter-dom, setup basic routing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <BrowserRouter>
+        <div className="App">
+          <Link to={"/"}>
+            <div className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <h2>Welcome to React</h2>
+            </div>
+          </Link>
+          <Route exact path="/" render={() => (
+            <div>
+              <h3>This is basic routing</h3>
+              <p><Link to={`/FirstPage`}>FirstPage</Link></p>
+              <p><Link to={`/SecondPage`}>SecondPage</Link></p>
+            </div>
+          )} />
+          <Route path="/firstpage" render={({ match }) => <div>This is FIRST page</div>} />
+          <Route path="/secondpage" render={({ match }) => <div>This is SECOND page</div>} />
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </BrowserRouter>
     );
   }
 }
